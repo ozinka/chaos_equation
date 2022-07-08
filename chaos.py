c155d74280110496389eb1441f8f80f5e72f0bb8
Vitaliy Osidach <vitaliy.osidach@ihsmarkit.com>
2022-07-08 12:29:50 +0300

    add numba for JIT, add check_fps decorator, add show fps

--- chaos.py
+++ chaos.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
+from numba import njit, prange
+from time import perf_counter
 
 
+@njit
 def chaos(n, r=1.0, pr_start=40):
-    x = pr_start/100
+    x = pr_start / 100
     for i in range(n):
+        i = i
         x = r * x * (1 - x)
-    return x*100
+    return x * 100
 
 
 fig, ax = plt.subplots()
@@ -27,7 +31,6 @@ l, = plt.plot(t, s, '-', color='red')
 
 k, = plt.plot(t, s, '.')
 
-
 ax.margins(x=0)
 
 ax_r = plt.axes([0.25, 0.1, 0.65, 0.03])
@@ -37,12 +40,26 @@ s_r = Slider(ax_r, 'R', 0.05, 5.0, valinit=r0, valstep=delta_f)
 s_pr_start = Slider(ax_pr_start, '% Start', 0, 100, valinit=pr_start0)
 
 
+# decorator to check FPS (1 / execution time)
+def check_fps(f):
+    def timed(*args, **kw):
+        start = perf_counter()
+        result = f(*args, **kw)
+        end = perf_counter()
+        fps.set_text(f"FPS: {1 / (end - start):>10,.2f}")
+        return result
+
+    return timed
+
+
+@check_fps
 def update(val):
-    pr_start = s_pr_start.val
+    pr_start = float(s_pr_start.val)
     r = s_r.val
     data = [chaos(xi, r, pr_start) for xi in t]
     l.set_ydata(np.array(data))
     k.set_ydata(np.array(data))
+
     fig.canvas.draw_idle()
 
 
@@ -51,6 +68,13 @@ s_pr_start.on_changed(update)
 
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', hovercolor='0.975')
+
+fps = ax.annotate('FPS:    0.00',
+                  xy=(1, 0),
+                  xycoords='axes fraction',
+                  fontsize=10,
+                  horizontalalignment='right',
+                  verticalalignment='bottom')
 
 
 def reset(event):
